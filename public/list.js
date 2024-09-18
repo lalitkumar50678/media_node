95cb4f5c255bf7c5086cc41c888d5ffdeb1fde84
lalit <lalitkumar50678@gmail.com>
2024-09-18 18:39:31 +0530

    List create in different file

---
+++ public/list.js
@@ -0,0 +1,228 @@
+// Sample media data (you can expand this)
+
+export const images = [
+  "images/image1.jpeg",
+  "images/image2.jpeg",
+  "images/image3.jpeg",
+  "images/image4.jpeg",
+  "images/image5.jpg",
+  "images/image6.jpg",
+  "images/image7.jpg",
+  "images/image8.jpg",
+  "images/image9.jpg",
+  "images/image10.jpg",
+  "images/image11.jpg",
+  "images/image12.jpg",
+  "images/image13.jpg",
+  "images/image14.jpg",
+  "images/image15.jpg",
+  "images/image16.jpg",
+  "images/image17.jpg",
+  "images/image18.jpg",
+  "images/image19.jpg",
+  "images/image20.jpg",
+  "images/image21.jpg",
+  "images/image22.jpg",
+  "images/image23.jpg",
+  "images/image24.jpg",
+  "images/image25.jpg",
+  "images/image26.jpg",
+  "images/image27.jpg",
+  "images/image28.jpg",
+];
+
+export const videos = [
+  "videos/video1.mp4",
+  "videos/video2.mp4",
+  "videos/video3.mp4",
+  "videos/video4.mp4",
+  "videos/video5.mp4",
+];
+
+export const mediaList = [
+  {
+    id: "110",
+    type: "image",
+    url: "images/image1.jpeg",
+  },
+  {
+    id: "111",
+    type: "image",
+    url: "images/image2.jpeg",
+  },
+  {
+    id: "112",
+    type: "image",
+    url: "images/image3.jpeg",
+  },
+  {
+    id: "113",
+    type: "image",
+    url: "videos/video.mp4",
+  },
+  {
+    id: "114",
+    type: "image",
+    url: "images/image4.jpeg",
+  },
+  {
+    id: "115",
+    type: "image",
+    url: "images/image5.jpeg",
+  },
+  {
+    id: "116",
+    type: "image",
+    url: "images/image6.jpeg",
+  },
+  {
+    id: "117",
+    type: "image",
+    url: "images/image7.jpeg",
+  },
+  {
+    id: "118",
+    type: "image",
+    url: "images/image8.jpeg",
+  },
+  {
+    id: "119",
+    type: "video",
+    url: "videos/video.mp4",
+  },
+  {
+    id: "120",
+    type: "image",
+    url: "images/image9.jpeg",
+  },
+  {
+    id: "121",
+    type: "image",
+    url: "images/image10.jpeg",
+  },
+  {
+    id: "122",
+    type: "image",
+    url: "images/image11.jpeg",
+  },
+  {
+    id: "123",
+    type: "image",
+    url: "images/image12.jpeg",
+  },
+  {
+    id: "124",
+    type: "image",
+    url: "images/image13.png",
+  },
+  {
+    id: "125",
+    type: "image",
+    url: "images/image14.jpeg",
+  },
+  {
+    id: "126",
+    type: "image",
+    url: "images/image15.jpeg",
+  },
+  {
+    id: "127",
+    type: "video",
+    url: "videos/video2.mp4",
+  },
+  {
+    id: "128",
+    type: "image",
+    url: "images/image16.jpeg",
+  },
+  {
+    id: "129",
+    type: "image",
+    url: "images/image17.jpg",
+  },
+  {
+    id: "130",
+    type: "image",
+    url: "images/image18.jpeg",
+  },
+  {
+    id: "131",
+    type: "video",
+    url: "videos/video2.mp4",
+  },
+  {
+    id: "132",
+    type: "video",
+    url: "videos/video3.mp4",
+  },
+  {
+    id: "133",
+    type: "image",
+    url: "images/image19.jpeg",
+  },
+  {
+    id: "134",
+    type: "image",
+    url: "images/image20.jpg",
+  },
+  {
+    id: "135",
+    type: "video",
+    url: "videos/video4.mp4",
+  },
+  {
+    id: "136",
+    type: "image",
+    url: "images/image21.jpeg",
+  },
+  {
+    id: "137",
+    type: "image",
+    url: "images/image22.jpg",
+  },
+  {
+    id: "138",
+    type: "video",
+    url: "videos/video5.mp4",
+  },
+  {
+    id: "139",
+    type: "image",
+    url: "images/image22.jpg",
+  },
+  {
+    id: "140",
+    type: "image",
+    url: "images/image23.jpg",
+  },
+  {
+    id: "140",
+    type: "image",
+    url: "images/image24.jpg",
+  },
+  {
+    id: "141",
+    type: "image",
+    url: "images/image25.jpg",
+  },
+  {
+    id: "142",
+    type: "image",
+    url: "images/image26.jpg",
+  },
+  {
+    id: "143",
+    type: "image",
+    url: "images/image27.jpg",
+  },
+  {
+    id: "144",
+    type: "image",
+    url: "images/image28.jpg",
+  },
+  {
+    id: "145",
+    type: "video",
+    url: "videos/video5.mp4",
+  },
+];
--- public/server.js
+++ public/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const { mediaList } = require("./list");
 
 const app = express();
 const PORT = process.env.PORT || 3002;
@@ -11,73 +12,28 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "")));
 // app.use(express.static(path.join(__dirname, "/public")));
 
-// Sample media data (you can expand this)
-const images = [
-  "images/image1.jpeg",
-  "images/image2.jpeg",
-  "images/image3.jpeg",
-  "images/image4.jpeg",
-  "images/image5.jpg",
-  "images/image6.jpg",
-  "images/image7.jpg",
-  "images/image8.jpg",
-  "images/image9.jpg",
-  "images/image10.jpg",
-  "images/image11.jpg",
-  "images/image12.jpg",
-  "images/image13.jpg",
-  "images/image14.jpg",
-  "images/image15.jpg",
-  "images/image16.jpg",
-  "images/image17.jpg",
-  "images/image18.jpg",
-  "images/image19.jpg",
-  "images/image20.jpg",
-  "images/image21.jpg",
-  "images/image22.jpg",
-  "images/image23.jpg",
-  "images/image24.jpg",
-  "images/image25.jpg",
-  "images/image26.jpg",
-  "images/image27.jpg",
-  "images/image28.jpg",
-];
-
-const videos = [
-  "videos/video1.mp4",
-  "videos/video2.mp4",
-  "videos/video3.mp4",
-  "videos/video4.mp4",
-  "videos/video5.mp4",
-];
-
 // Pagination parameters
-const ITEMS_PER_PAGE = 2;
+const ITEMS_PER_PAGE = 4;
 
-app.get("/api/images", (req, res) => {
+app.get("/api/getMedia", (req, res) => {
   const page = parseInt(req.query.page) || 1;
-  const totalImages = images.length;
-  const totalPagesImages = Math.ceil(totalImages / ITEMS_PER_PAGE);
-  const imageStartIndex = (page - 1) * ITEMS_PER_PAGE;
+  const totalMedia = mediaList.length;
+  const totalPagesMedia = Math.ceil(totalMedia / ITEMS_PER_PAGE);
+  const mediaStartIndex = (page - 1) * ITEMS_PER_PAGE;
 
   const baseUrl = `${req.protocol}://${req.get("host")}`;
-  const paginatedImages = images
-    .slice(imageStartIndex, imageStartIndex + ITEMS_PER_PAGE)
+  const paginatedMedia = mediaList
+    .slice(mediaStartIndex, mediaStartIndex + ITEMS_PER_PAGE)
     .map((image) => {
       return `${baseUrl}/${image}`;
     });
 
   res.json({
-    images: paginatedImages,
+    images: paginatedMedia,
     dirname: path.join(__dirname, "/images"),
     currentPage: page,
-    totalPages: totalPagesImages,
+    totalPages: totalPagesMedia,
   });
-});
-
-app.get("/test", (req, res) => {
-  res.sendFile(path.join(__dirname, "images/image1.jpeg"));
-  console.log(path.join(__dirname, "images/image1.jpeg"));
 });
 
 app.get("/api/videos", (req, res) => {
