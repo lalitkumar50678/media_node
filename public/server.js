d0c232ca48f42c917611c6d012da6c4a4f540c24
lalit <lalitkumar50678@gmail.com>
2024-09-19 16:15:23 +0530

    pathname change to public

--- public/server.js
+++ public/server.js
@@ -20,7 +20,7 @@ app.get("/api/getMedia", (req, res) => {
   const paginatedMedia = mediaList
     .slice(mediaStartIndex, mediaStartIndex + ITEMS_PER_PAGE)
     .map((item) => ({ ...item, url: `${baseUrl}/${item.url}` }));
-  path.join(__dirname, "");
+  path.join(__dirname, "public");
 
   res.json({
     images: paginatedMedia,
