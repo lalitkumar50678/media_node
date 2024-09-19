286f8ca2902b35882bcd2783fe330e94db476d8b
lalit <lalitkumar50678@gmail.com>
2024-09-19 16:06:25 +0530

    server.js update

--- public/server.js
+++ public/server.js
@@ -24,7 +24,7 @@ app.get("/api/getMedia", (req, res) => {
   res.json({
     images: paginatedMedia,
     currentPage: page,
-    dirname: path.join(__dirname, "images"),
+    dirname: path.join(__dirname, ""),
     totalPages: totalPagesMedia,
   });
 });
