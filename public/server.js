30e6d977926af9f65ce7ada0bde0f8f0976a4950
lalit <lalitkumar50678@gmail.com>
2024-09-19 16:20:24 +0530

    slash added in pathname

--- public/server.js
+++ public/server.js
@@ -20,12 +20,12 @@ app.get("/api/getMedia", (req, res) => {
   const paginatedMedia = mediaList
     .slice(mediaStartIndex, mediaStartIndex + ITEMS_PER_PAGE)
     .map((item) => ({ ...item, url: `${baseUrl}/${item.url}` }));
-  path.join(__dirname, "images");
+  path.join(__dirname, "/");
 
   res.json({
     images: paginatedMedia,
     currentPage: page,
-    dirname: path.join(__dirname, ""),
+    dirname: path.join(__dirname, "/"),
     totalPages: totalPagesMedia,
   });
 });
